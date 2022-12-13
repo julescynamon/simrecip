abf56912631588df28d4ff1915dff53a24b4ae3e
cynamon jules <julescweb@mail.com>
2022-12-13 12:49:05 +0100

    petite ameliroation pour la page ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -11,7 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
